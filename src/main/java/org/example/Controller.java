e7df4bf300cbbf3b8e07abd944309c69d0609fe7
a.masterenko <andrey.masterenko@stoloto.ru>
2023-11-20 18:43:20 +0300

    Add demo app

---
+++ src/main/java/org/example/Controller.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger("Controller");
+
+    @GetMapping("/data")
+    public ResponseEntity<String> getData() {
+        LOGGER.info("Sending response");
+        return ResponseEntity.ok("123");
+    }
+}
